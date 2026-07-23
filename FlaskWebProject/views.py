2a4ad83a23d253b62d06a281496edfeb1840e8cc
pikkili sowmya <pikkilisowmya277@gmail.com>
2026-07-23 18:53:35 +0530

    Replace print statements with logger warnings

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -66,10 +66,10 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            print("Invalid login attempt")
+            app.logger.warning("Invalid login attempt")
             flash('Invalid username or password')
             return redirect(url_for('login'))
-        print("admin logged in successfully")
+        app.logger.warning("admin logged in successfully")
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
